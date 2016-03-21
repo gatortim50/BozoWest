ea4c9b52734834e4ef3fe5eb8410d05e310ba6c4
Timothy McKee <timothy.mckee@viawest.com>
2016-03-21 01:10:56 -0400

    updated Jobs route

--- server/models/Jobs.js
+++ server/models/Jobs.js
@@ -1,6 +1,14 @@
 
 // Jobs.js
 
+var Jobs = [
+  {id: 144, name: 'Colo & Network Services'},
+  {id: 227, name: 'Cloud'},
+  {id: 325, name: 'Security'},
+  {id: 400, name: 'AppliedTrust Consulting'},
+  {id: 501, name: 'Data Solutions'}
+];
+
 exports.findProduct = function (req, res) {
   var rs = req.params.id;
   //res.send({product: rs});
@@ -10,20 +18,22 @@ exports.findProduct = function (req, res) {
 };
 
 exports.findById = function (req, res) {
-  //res.send({id: req.params.id, name: "The Test Name", description: "The Test Description"});
 
-  return ([
-    {id: req.params.id, name: 'Data Solutions'}
-  ]);
+  var len = Jobs.length;
+  var myArray = [];
+
+  for (var i = 0; i < len; i ++) {
+    var result = Jobs[i];
+
+    if (result.id == req.params.id) {
+      myArray.push(result);
+      return myArray;
+    }
+  }
+
 };
 
 exports.findAll = function (req, res) {
   //console.log('in findAll');
-  return ([
-    {id: 144, name: 'Colo & Network Services'},
-    {id: 227, name: 'Cloud'},
-    {id: 325, name: 'Security'},
-    {id: 400, name: 'AppliedTrust Consulting'},
-    {id: 501, name: 'Data Solutions'}
-  ]);
+  return (Jobs);
 };
